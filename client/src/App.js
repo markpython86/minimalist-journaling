4a2c878cffef16b46bbf3eadb21a537fabfdb09b
Mark <mark.faraj86@gmail.com>
2019-04-10 15:57:44 -0500

    tweeks

--- client/src/App.js
+++ client/src/App.js
@@ -45,30 +45,30 @@ ReactDOM.render(
               <Home />
             )
           )}/> */}
-          <Route exact path="/signin" render={() => (
+          <Route exact path="/minimalist-journaling/signin" render={() => (
             token ? (
-              <Redirect to="/daily"/>
+              <Redirect to="/minimalist-journaling/daily"/>
             ) : (
               <Signin />
             )
           )}/>
-          <Route exact path="/signup" render={() => (
+          <Route exact path="/minimalist-journaling/signup" render={() => (
             token ? (
-            <Redirect to="/daily"/>
+            <Redirect to="/minimalist-journaling/daily"/>
             ) : (
               <Signup />
             )
           )}/>
-          <Route exact path="/" component= {Home} />
+          <Route exact path="/minimalist-journaling/" component= {Home} />
           {/* <Route path="/public" component= {Public} /> */}
-          <Route path="/account" component= {RequireAuth(Account)} />
+          <Route path="/minimalist-journaling/account" component= {RequireAuth(Account)} />
 
-          <Route path="/about" component= {About} />
+          <Route path="/minimalist-journaling/about" component= {About} />
           
-          <Route path="/signout" component= {Signout} />
-          <Route path="/daily" component= {RequireAuth(Daily)} />
-          <Route path="/weekly" component= {RequireAuth(Weekly)} />
-          <Route path="/monthly" component= {RequireAuth(Monthly)} /> 
+          <Route path="/minimalist-journaling/signout" component= {Signout} />
+          <Route path="/minimalist-journaling/daily" component= {RequireAuth(Daily)} />
+          <Route path="/minimalist-journaling/weekly" component= {RequireAuth(Weekly)} />
+          <Route path="/minimalist-journaling/monthly" component= {RequireAuth(Monthly)} /> 
           {/* <Route path="/ui" component= {App1} /> */}
           
 
