05eb4bfb5c534d0a7df15c1e60a41ce1c4fbf415
Mark <mark.faraj86@gmail.com>
2019-04-09 21:58:25 -0500

    trying to deploy

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -4,12 +4,16 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.join(__dirname, './src'),
+        compress: true,
+        port: 3000
+    },
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production'),
-                
-
                 'API_URI': JSON.stringify('https://minimalist-journaling.herokuapp.com/')
             },
         }),
