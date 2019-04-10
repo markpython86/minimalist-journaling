902757261e8895a36a3892956583ee9fef25c6eb
Mark <mark.faraj86@gmail.com>
2019-04-09 22:03:11 -0500

    trying to deploy

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-
+const path = require('path');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
